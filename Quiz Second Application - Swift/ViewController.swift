887735e1d7b9b64765f7ba46abee2ea5d7c98477
ElijahButers <Elijahbuters@gmail.com>
2017-02-27 18:21:18 +0200

    Connected two new labels to the ViewController class as outlets.

--- Quiz Second Application - Swift/ViewController.swift
+++ Quiz Second Application - Swift/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var highScoreLabel: UILabel!
     
 
     override func viewDidLoad() {

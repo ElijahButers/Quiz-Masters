e6304b1e620e72b4030419193b126b49b9d9dc27
ElijahButers <Elijahbuters@gmail.com>
2017-03-08 11:53:47 +0200

    Added code to the viewDidLoad() in the MultipleChoiceViewController that shuffles the answers on the answer buttons when they're downloaded.

--- MultipleChoiceViewController.swift
+++ MultipleChoiceViewController.swift
@@ -68,6 +68,7 @@ class MultipleChoiceViewController: UIViewController {
         cardButton.isEnabled = false
         
         mcArray.shuffle()
+        answerButtons.shuffle()
         nextQuestion()
     }
 
